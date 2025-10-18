e629203e6d91f8b7dd23edcb55f53a65a2824d9b
Besp1N <kackar87@wp.pl>
2025-10-18 22:08:00 +0200

    initial commit

---
+++ NotificationCenter.Api/Program.cs
@@ -0,0 +1,22 @@
+using System.Reflection.Metadata;
+using MediatR;
+using FluentValidation;
+using NotificationCenter.Application;
+using NotificationCenter.Application.DependencyInjection;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddApplication();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.Run();
